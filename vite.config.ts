fac56222e6989e066a7553c483f2c7e97951a8b0
Aashish Bam <taashish848@gmail.com>
2025-01-24 09:58:45 +0545

    fix: update module handling and MIME types

--- vite.config.ts
+++ vite.config.ts
@@ -10,11 +10,16 @@ export default defineConfig({
     },
   },
   build: {
+    modulePreload: true,
+    target: 'esnext',
+    minify: 'esbuild',
     rollupOptions: {
       output: {
+        manualChunks: undefined,
         format: 'es',
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
       },
     },
   },
